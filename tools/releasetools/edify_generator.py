2828781901f33404f723004b43fc2d402da6f7df
Steve Kondik <shade@chemlab.org>
2010-04-21 11:39:48 -0400

    Allow override of device asserts, including multi-device support.
    Set in board file with TARGET_OTA_ASSERT_DEVICE.
    (cherry-picked from commit 0f452f21fc9323b9d1fe746161761cf40aaa5030)
    
    Change-Id: I3d06bdc0e3e26bde0c0e646accd050364f9713b9
    
    Conflicts:
    	core/Makefile
    	tools/releasetools/edify_generator.py
    	tools/releasetools/ota_from_target_files
    
    Conflicts:
    	core/Makefile
    	tools/releasetools/edify_generator.py
    	tools/releasetools/ota_from_target_files

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -92,12 +92,10 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    cmd = ('(' +
+    cmd = ('assert(' +
            ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
                          % (i, i) for i in device.split(",")]) +
-           ') || abort("This package is for \\"%s\\" devices\n'
-           'this is a \\"" + getprop("ro.product.device") + "\\".");'
-           ) % (device)
+           ');')
     self.script.append(self._WordWrap(cmd))
 
   def AssertSomeBootloader(self, *bootloaders):
