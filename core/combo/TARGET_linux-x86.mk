90abdd46895bb3f670b826697cd3823484ffed18
PrimeDirective <activethrasher00@gmail.com>
2013-12-11 15:28:25 -0500

    disable fortify source
    From what i understand: glibc's fortify source attempts the use of some
    non-optimized string routines as buffer-length aware replacements.
    It also incures additional performance losses by inserting and verifying
    canaries before/after the stack to protect against buffer overflow attacks.
    I don't think we have experienced this problem and flags can be tailored to
    each device tree if we run into it.
    
    Change-Id: Iac4f5b101f24923a33ffa6367dbb992717eac0e9

--- core/combo/TARGET_linux-x86.mk
+++ core/combo/TARGET_linux-x86.mk
@@ -92,7 +92,7 @@ TARGET_GLOBAL_CFLAGS += \
 			-Ulinux \
 			-Wa,--noexecstack \
 			-Werror=format-security \
-			-D_FORTIFY_SOURCE=2 \
+			-D_FORTIFY_SOURCE=0 \
 			-Wstrict-aliasing=2 \
 			-fPIC -fPIE \
 			-ffunction-sections \
